5f3dbb7e021f7e16bc4dd56e9190375dce4875d8
AhmedElsayed6 <amabrook2022@gmail.com>
2024-07-22 16:12:33 +0300

    Removed everything from GITITIT.JAVA

--- GITITI.java
+++ GITITI.java
@@ -1,17 +1,4 @@
 public class HelloITI {
 
-	public void PrintHelloITI(){
-	
-	System.out.print("Hi Eid");
-	
-	}
-	
-	public static void main (String args){
-		
-		
-	}
-	
-	public void printHelloGit(){
-		System.out.print("Hello GIT");
-	}
+
 }
